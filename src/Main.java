2c9bf21b327e1907f0d3e2780850655ecd1e5b4f
RoMANzhula <roman1985al@ukr.net>
2023-04-20 00:24:29 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String fileNameFromReader = "example_C:/fileFrom.txt"; //создаем строковую переменную - имя файла, из которого будем считывать
         String fileNameToWriter = "example_C:/fileTo.txt"; //создаем строковую переменную - имя файла, в который будем записывать
+        String searchSameWordInFile = "example"; //создаем строковую переменную и инициализируем ее словом, которое хотим искать
 
         charFromFistFileToSecondFile(fileNameFromReader, fileNameToWriter); //вызываем метод копирования из файла в файл
         copyOnlyEvenCharacters(fileNameFromReader, fileNameToWriter); //вызываем метод для копирования четных символов из файла в файл
+        outputTheNumberOfTheSameWord(fileNameFromReader, searchSameWordInFile); //вызываем метод, который выводит в консоль
+        //количество заданных нами одинаковых слов в указанном файле
     }
 
     public static void charFromFistFileToSecondFile(String fileNameFromReader, String fileNameToWriter) throws IOException { //метод, который копирует символы из одного текстового
@@ -27,7 +30,8 @@ public class Main {
         fileWriter.close(); //очищаем ресурсы, закрываем поток вывода(записи) символов
     }
 
-    public static void copyOnlyEvenCharacters(String fileNameFromReader, String fileNameToWriter) {
+    public static void copyOnlyEvenCharacters(String fileNameFromReader, String fileNameToWriter) { //метод, который копирует
+        //четные символы из одного файла в другой файл
         try (FileReader fileReader = new FileReader(fileNameFromReader); //открываем поток для ввода(считывания) символьных(текстовых) данных
         FileWriter fileWriter = new FileWriter(fileNameToWriter)) {//открываем поток для вывода(записи) символьных(текстовых) данных
 
@@ -43,6 +47,36 @@ public class Main {
             }
 
         } catch (IOException e) { //ловим исключения входных/выходных потоков
+            e.printStackTrace(); //пишем исключение в стек
+        }
+    }
+
+    public static void outputTheNumberOfTheSameWord(String fileNameFromReader, String sameWord) { //метод, который выведет
+        // количество указанных одинаковых слов из указанного файла
+        try (FileReader fileReader = new FileReader(fileNameFromReader)) { //в блоке try-with-resource открываем поток для
+            //чтения символов из указанного файла
+            StringBuilder stringBuilder = new StringBuilder(); //создаем обьект типа StringBuilder
+            int counterForConsoleOutput = 0; //создаем переменную-счетчик символов для записи
+
+            while (fileReader.ready()) { //пока есть символы для чтения из файла
+                stringBuilder.append((char) fileReader.read()); //формируем строку из считанных симолов
+            }
+
+            String fromBuilderToString = stringBuilder.toString(); //строку из символов преобразовываем к строковому виду
+
+            String[] arrayOfWords = fromBuilderToString.split("[^A-Za-z0-9]"); //разделяем строку на слова, которые мы
+            //определяем в массив строк
+
+            for (int i = 0; i < arrayOfWords.length; i++) { //проходимся по массиву слов
+                if (arrayOfWords[i].equals(sameWord)) { //если слово из массива слов будет совпадать с искомым словом, то
+                    counterForConsoleOutput++; //увеличиваем счетчик
+                }
+            }
+
+            System.out.println("Number of the same words: " + sameWord + "in file = " + counterForConsoleOutput); //выводим
+            //количество совпадающих слов
+
+        } catch (IOException e) {//ловим исключения входных/выходных потоков
             e.printStackTrace(); //пишем исключение в стек
         }
     }
